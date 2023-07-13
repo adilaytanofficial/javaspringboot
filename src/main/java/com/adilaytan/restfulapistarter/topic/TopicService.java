e156a49c4b2fb4a912a9c8f84bfe0bf47b3d3ceb
Adil AYTAN <adilaytanofficial@gmail.com>
2023-07-13 22:26:54 +0300

    Creating New Resource Using Post

--- src/main/java/com/adilaytan/restfulapistarter/topic/TopicService.java
+++ src/main/java/com/adilaytan/restfulapistarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.adilaytan.restfulapistarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "JavaScript", "JavaScript Description"));
+			new Topic("javascript", "JavaScript", "JavaScript Description")));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -18,6 +19,10 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(x -> x.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 	
 }
